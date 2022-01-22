24ce3cd42713e1793f4bb15a03c312abe89aca1d
SHAIKH MANIK <shaikhmanik@SHAIKHs-MacBook-Pro.local>
2022-01-22 22:01:53 +0530

    added some fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,6 @@
-import Head from 'next/head'
-import { useEffect, useState } from 'react';
+import '../styles/style.css';
 
-import '../styles/style.css'
-import Navbar from '../components/Navbar'
-import About from '../components/About'
-import Animation from '../components/Animation';
-
-export default function App() {
-
-  const themes = ['light' , 'dark']
-  const [theme, setTheme] = useState('dark')
-  const changeTheme = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light')
-  }
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => setLoading(true),[]);
-
-  {/* <Head> */}
-  {/* <link rel="preconnect" href="https://fonts.googleapis.com"/>
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;500&display=swap" rel="stylesheet"/> */}
-  {/* </Head> */}
-  return (
-    <div className="dark wholebody">
-       {loading && <Animation/> }
-       <h6>Shaikh Manik</h6>
-       <p>Use mouse to move in space</p>
-    </div>
-  )
+function Website({ Component, pageProps, router }) {
+  return <Component {...pageProps} key={router.route} />
 }
+export default Website
